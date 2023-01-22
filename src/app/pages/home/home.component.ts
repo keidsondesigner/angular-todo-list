7b3ea41da078b24f0d5d597e8596145a218d7eab
keidsondesigner <keidsondesigner@gmail.com>
2023-01-22 17:13:12 -0400

    criação do service e metodos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/service/data.service';
+import { Todo } from './../../model/todo.model';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  todos: Todo[] = [];
 
-  constructor() { }
+  constructor(private todoDataService: DataService) { }
 
   ngOnInit(): void {
+    this.todos = this.todoDataService.getAllTodos();
+  }
+
+  editTodo(){
+    console.log('Editando');
+  }
+
+  deleteTodo(){
+    console.log('Deletedo');
   }
 }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  todos: Todo[] = [
+    new Todo('Um teste aqui', false),
+    new Todo('Outro teste', true),
+  ];
+
   constructor() { }
+
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  addTdod(todo: Todo){
+    this.todos.push(todo);
+  }
+
+  updateTodo(index: number, updatedTodo: Todo) {
+    this.todos[index] = updatedTodo;
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
