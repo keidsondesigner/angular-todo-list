09f4c44b1494fc858194a208c923f4c358312927
keidsondesigner <keidsondesigner@gmail.com>
2023-02-26 20:40:53 -0400

    criado component edit-todo-dialog

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { DataService } from 'src/app/service/data.service';
+import { EditTodoDialogComponent } from 'src/app/shared/components/edit-todo-dialog/edit-todo-dialog.component';
 import { Todo } from './../../model/todo.model';
 
 @Component({
@@ -13,7 +15,10 @@ export class HomeComponent implements OnInit {
 
   showValidationErrors: boolean = false;
 
-  constructor(private _todoDataService: DataService) { }
+  constructor(
+    private _todoDataService: DataService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.todos = this._todoDataService.getAllTodos();
@@ -40,9 +45,20 @@ export class HomeComponent implements OnInit {
   }
 
   editTodo(todo: Todo) {
-    // pegando o index de cada todo do array de todos;
+    // pegando o index de cada "todo" do array de todos;
     const index = this.todos.indexOf(todo);
-    // this._todoDataService.updateTodo()
+
+    let dialogRef = this.dialog.open(EditTodoDialogComponent, {
+      width: '700px',
+      data: todo
+    });
+
+    dialogRef.afterClosed().subscribe((resultado) => {
+      console.log("resultado", resultado);
+      if(resultado) {
+        this._todoDataService.updateTodo(index, resultado);
+      }
+    })
   }
 
   deleteTodo(todo: Todo){
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { EditTodoDialogComponent } from './components/edit-todo-dialog/edit-todo-dialog.component';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 @NgModule({
   declarations: [
-    TodoItemComponent
+    TodoItemComponent,
+    EditTodoDialogComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MatDialogModule,
+    MatInputModule
   ],
   exports: [
-    TodoItemComponent
+    TodoItemComponent,
+    EditTodoDialogComponent,
+    MatDialogModule
+  ],
+  providers: [
+    { provide: MAT_DIALOG_DATA, useValue: {} },
+    { provide: MatDialogRef, useValue: {} }
   ]
 })
 export class SharedModule { }
